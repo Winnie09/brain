f5c5c4e942f830dfe112e345fecffc78e59dc982
Wenpin Hou <wp.hou3@gmail.com>
2021-05-10 16:26:05 -0400

    02_plot.R

--- humanref/code/02_plot.R
+++ humanref/code/02_plot.R
@@ -3,11 +3,15 @@ ser <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/humanref/res/integra
 ref <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/humanref/res/reference.rds')
 
 meta <- ser@meta.data
-rownames(meta) <- 
+
 ct <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/integrate/harmony/36nonNormalSeuratGene2000/res/celltype_annotation.rds')  
 ser@meta.data <- cbind(meta, celltype=ct[rownames(meta)])
 
-p1 = DimPlot(ref, reduction = "ref.umap", group.by = "Cell.Type", label = TRUE, label.size = 3, repel = TRUE) + NoLegend()
-p2 = DimPlot(ser, reduction = "ref.umap", group.by = "predicted.celltype.l2", label = TRUE, label.size = 3 ,repel = TRUE) + NoLegend()
+p1 = DimPlot(ref, reduction = "umap", group.by = "Cell.Type", label = TRUE, label.size = 3, repel = TRUE) + NoLegend()
+p2 = DimPlot(ser, reduction = "ref.umap", group.by = "celltype", label = TRUE, label.size = 3 ,repel = TRUE) + NoLegend()
 p1 + p2
+
+png('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/humanref/plot/alltogether_cortex_v3_celltype2.png', width = 1800, height = 1400, res = 250)
+DimPlot(reference, reduction = "umap", group.by = "Cell.Type", label = F, label.size = 3 ,repel = TRUE) #+ NoLegend()
+dev.off()
 
