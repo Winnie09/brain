46c2d412aed3af13df2d48225b110269bc1ac784
Wenpin Hou <wp.hou3@gmail.com>
2021-05-10 16:26:00 -0400

    01_integrate.R

--- humanref/code/01_integrate.R
+++ humanref/code/01_integrate.R
@@ -8,7 +8,7 @@ reference <- RunPCA(reference)
 reference <- RunUMAP(reference,dims=1:50,return.model=TRUE)
 saveRDS(reference, '/home-4/whou10@jhu.edu/scratch/Wenpin/brain/humanref/res/reference.rds')
 # $ : chr [1:19368] "FO538757.2" "AP006222.2" "RP4-669L17.10" "RP5-857K21.4" ...
-# $ : chr [1:58145] 
+# $ : chr [1:58145]
 
 target <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/integrate/harmony/36nonNormalSeuratGene2000/res/humanAtlas_harmony.rds')
 #Idents(target) <- 'orig.ident'
@@ -29,6 +29,4 @@ target <- MapQuery(
   reduction.model = "umap"
 )
 saveRDS(target,file='/home-4/whou10@jhu.edu/scratch/Wenpin/brain/humanref/res/integrate.rds')
-
-
 
