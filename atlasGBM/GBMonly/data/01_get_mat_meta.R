b58a0f260cda20de6393bce0038e4f6c8a73432d
Wenpin Hou <wp.hou3@gmail.com>
2021-07-13 11:29:12 -0400

    01_get_mat_meta.R

--- atlasGBM/GBMonly/data/01_get_mat_meta.R
+++ atlasGBM/GBMonly/data/01_get_mat_meta.R
@@ -25,3 +25,22 @@ meta <- read.csv('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/doc/metas_20200329.
 cm <- data.frame(cell=colnames(mat),study='GBM',meta[match(sub('_.*','',colnames(mat)),meta[,1]),])
 saveRDS(cm, 'atlasGBM/GBMonly/data/36nonNormal_combined_meta.rds')
 
+
+### 6 normal samples
+cnt = readRDS('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/proc/tumor/matrix/count.rds')
+meta.cnt = read.csv('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/doc/metas_20200329.csv', as.is = T)
+s = meta.cnt[meta.cnt$Pathology == 'Normal', 1]
+as = sub('_.*', '', colnames(cnt))
+cnt = cnt[, as %in% s]
+
+mat <- log2(t(t(cnt)/(colSums(cnt) / 1e6) + 1))
+
+as = sub('_.*', '', colnames(mat))
+
+dlist = list()
+for (i in unique(as)){
+  dlist[[i]] <- mat[, as == i]
+}
+saveRDS(dlist, 'atlasGBM/GBMonly/data/6Normal_combined_log2norm_dlist.rds')
+
+
