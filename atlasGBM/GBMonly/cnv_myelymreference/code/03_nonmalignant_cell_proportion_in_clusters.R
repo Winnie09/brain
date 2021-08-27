8a103c981e52c923dfaf853697b26fe5ee57e8a0
Wenpin Hou <wp.hou3@gmail.com>
2021-08-27 12:00:50 -0400

    03_nonmalignant_cell_proportion_in_clusters.R

--- atlasGBM/GBMonly/cnv_myelymreference/code/03_nonmalignant_cell_proportion_in_clusters.R
+++ atlasGBM/GBMonly/cnv_myelymreference/code/03_nonmalignant_cell_proportion_in_clusters.R
@@ -1,5 +1,6 @@
 list = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/cnv_myelymreference/res/nonmaglinant_cells.rds')
 cell = unlist(list)
+cell = cell[!is.na(cell)]
 clu = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/integrate/seurat/36nonNormalSeuratGene2000/res/cluster.rds')
 patient = gsub('_.*', '', names(clu))  
 
@@ -10,3 +11,14 @@ t2 <- table(clusub)
 res = sort(t1/t2[names(t1)])
 write.csv(res, '/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/cnv_myelymreference/res/nonmalignant_cell_proportion_in_clusters.csv')
 
+library(ggplot2)
+pdf('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/cnv_myelymreference/plot/nonmalignant_proportion_in_clusters.pdf', width = 4, height = 3.5)
+pd = data.frame(cluster = paste0('cluster ', names(res)), prop = as.numeric(res), stringsAsFactors = F)
+pd[,1] = factor(pd[,1], levels = pd[,1])
+
+ggplot(data = pd, aes(x = cluster, y = prop)) +
+  geom_col() +
+  ylab('non-malignant cells proportion') +
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 90))
+dev.off()
