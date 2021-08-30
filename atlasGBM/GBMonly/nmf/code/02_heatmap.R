a4b8f2a28b0a60f1b773272dbebd1a36324786f5
Wenpin Hou <wp.hou3@gmail.com>
2021-08-29 21:28:17 -0400

    02_heatmap.R: nmf plot gene heatmap, cluster genes first

--- atlasGBM/GBMonly/nmf/code/02_heatmap.R
+++ atlasGBM/GBMonly/nmf/code/02_heatmap.R
@@ -1,28 +1,34 @@
 library(pheatmap)
+library(Matrix)
 path <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/nmf/res/overallgenepathway.rds')
 ng <- nrow(path)
 path <- as.vector(path)
 
-expr <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/data/36nonNormal_combined_log2norm_mat.rds')
-
+expr <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/integrate/seurat/36nonNormalSeuratGene2000/res/norm.rds')
 af <- list.files('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/cellcellcor/res',pattern = 'cellorder')
+
+ord <- sapply(af, function(f){
+  readRDS(paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/cellcellcor/res/',f))
+})
+ord.all = unlist(ord)
+
+ord.gene = sapply(1:4, function(i){
+  expr1 = expr[path[(1+30*(i-1)):(30*i)], ord.all]
+  expr1 = expr1[, colSums(expr1)>0]
+  pr = prcomp(expr1, scale. = T)$x
+  cut = sort(cutree(hclust(dist(pr)), k = 5))
+  names(cut)
+})
+ord.gene = as.vector(ord.gene)
+
 for (f in af) {
   order <- readRDS(paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/cellcellcor/res/',f))
-  
-  pd <- expr[path,order]
+  pd <- expr[ord.gene,order]
   samp <- sub('_.*','',colnames(pd))
   for (s in unique(samp))
     pd[,samp==s] <- (pd[,samp==s]-rowMeans(pd[,samp==s]))
-  
-  # order <- unlist(sapply(unique(samp),function(s) {
-  #   colnames(pd)[samp==s][hclust(dist(t(pd[,samp==s])))$order]
-  # }))
-  # pd <- pd[,order]
-  
-  # pd[pd > quantile(pd,0.95)] <- quantile(pd,0.95)
-  # pd[pd < quantile(pd,0.05)] <- quantile(pd,0.05)
   png(paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/nmf/plot/heatmap/',sub('cellorder_','',sub('.rds','',f)),'.png'),width=1000,height=1500,res=100)
   pheatmap(pd,cluster_rows = F,cluster_cols = F,show_colnames = F,color=colorRampPalette(c('blue','cyan','white','pink','red'))(20),gaps_col = cumsum(rle(sub('_.*','',colnames(pd)))$lengths),gaps_row = c(1:4)*30)
-  dev.off()  
+  dev.off()
 }
 
