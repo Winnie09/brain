abfb1f35c7fbef9fb1e2923ee3afe1a7edf4616a
Wenpin Hou <wp.hou3@gmail.com>
2022-05-07 15:53:16 -0400

    04_compare_cnvclu_with_GBMclu.R

--- atlasGBM/GBMonly/cnv_myelymreference/code/04_compare_cnvclu_with_GBMclu.R
+++ atlasGBM/GBMonly/cnv_myelymreference/code/04_compare_cnvclu_with_GBMclu.R
@@ -9,7 +9,13 @@ for (f in af) {
   cnv <- readRDS(paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/cnv_myelymreference/cnvclu/',f))
   df <- data.frame(table(cnv,ct[names(cnv)]))
   colnames(df) <- c('cnv','expr','count')
-  pdf(paste0(pdir, sub('.rds','.pdf', f)), width = 4, height=4)
-  print(ggplot(df,aes(x=cnv,y=expr,fill=count)) + geom_tile() + theme_classic() + scale_fill_gradient(low='black',high='yellow') + xlab('cnv clusters') + ylab('expression clusters'))
+  df[,1] <- as.character(df[,1])
+  tab = table(cnv)
+  df[,1] <- paste0(df[,1],'(', tab[df[,1]],')')
+  df[,1] <- factor(df[,1], levels = unique(df[,1]))
+  
+  pdf(paste0(pdir, sub('.rds','.pdf', f)), width = ifelse(f %in% c('GBM074.rds'), 5,4), height=4)
+  print(ggplot(df,aes(x=cnv,y=expr,fill=count)) + geom_tile() + theme_classic() + scale_fill_gradient(low='black',high='yellow') + xlab('cnv clusters') + ylab('expression clusters') + theme(axis.text.x = element_text(angle=45, hjust = 1)))
   dev.off()
 }
+
