59bdb85fdda5f2e50001214ee180b57b523f3634
Wenpin Hou <wp.hou3@gmail.com>
2022-05-07 19:40:18 -0400

    04_meta.R

---
+++ atlasGBM/GBMonly/integrate/seurat/36nonNormalSeuratGene2000/code/04_meta.R
@@ -0,0 +1,4 @@
+meta <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/integrate/harmony/36nonNormalSeuratGene2000/res/meta.data.rds')
+clu <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/integrate/seurat/36nonNormalSeuratGene2000/res/cluster.rds')
+meta <- meta[names(clu),]
+saveRDS(meta,file='/home-4/whou10@jhu.edu/scratch/Wenpin/brain/atlasGBM/GBMonly/integrate/seurat/36nonNormalSeuratGene2000/res/meta.rds')
