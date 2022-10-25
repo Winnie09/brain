205de370dec91ee02632f6c51885c55471e61c85
whou10 <wp.hou3@gmail.com>
2022-10-24 22:26:51 -0400

    scRNA_pb.R

--- predDNAm/data/sep/GSE151506_hg38/code/scRNA_pb.R
+++ predDNAm/data/sep/GSE151506_hg38/code/scRNA_pb.R
@@ -15,3 +15,13 @@ m <- m/as.vector(table(s)[rownames(m)])
 m <- t(m)
 saveRDS(m,file='/home/whou10/scratch16/whou10/brain/predDNAm/data/sep/GSE151506_hg38/proc/scRNA/saver_log2tpm_pb.rds')
 
+## retrain pseudobulk of >=5 cells
+d <- readRDS('/home/whou10/scratch16/whou10/brain/predDNAm/data/sep/GSE151506_hg38/proc/imp/scRNA_paired_saver_log2tpm.rds')
+s <- sub(':.*','',colnames(d))
+m <- rowsum(t(d),s)
+m <- m/as.vector(table(s)[rownames(m)])
+m <- t(m)
+tab <- table(s)
+m2 <- m[,names(tab)[tab >= 5]]
+saveRDS(m2,file='/home/whou10/scratch16/whou10/brain/predDNAm/data/sep/GSE151506_hg38/proc/scRNA/saver_log2tpm_pb_atLeast5Cell.rds')
+
